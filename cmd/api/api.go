ae3d1e04879cb5e0f27fbf782db783bceeda697c
patrykpartyka <patryk.partyka@flyrlabs.com>
2025-01-02 18:22:03 +0100

    feat: api initial setup

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,44 @@
-package api
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// handlers ...
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+
+	// posts
+	// users
+	// auth
+
+	return mux
+}
+
+func (app *application) run() error {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at %s", app.config.addr)
+
+	return server.ListenAndServe()
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,15 @@
-package api
+package main
+
+import "log"
+
+func main() {
+	cfg := &config{
+		addr: ":8080",
+	}
+
+	app := &application{
+		config: *cfg,
+	}
+
+	log.Fatal(app.run())
+}
